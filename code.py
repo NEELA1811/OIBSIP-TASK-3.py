3ff8296877a7e7680347e9ebf9392eddc5a23f9b
NEELA1811 <157453205+NEELA1811@users.noreply.github.com>
2024-02-02 20:02:22 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,28 @@
 import requests
+import json
 
-api_key = 'YOUR_API_KEY'
+base_url = "https://api.openweathermap.org/data/2.5/weather?"
 
-city = input('Enter city name: ')
+city = input("Enter a city to get weather details : ")
+print("")
 
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+api_key = "a49de69b810181cc6659a996563a51dc"
 
+url= base_url + "q=" + city + "&appid=" + api_key
 response = requests.get(url)
 
 if response.status_code == 200:
-    data = response.json()
-    temp = data['main']['temp']
-    desc = data['weather'][0]['description']
-    print(f'Temperature: {temp} K')
-    print(f'Description: {desc}')
+   data = response.json()
+   main = data['main']
+   temp = main['temp']
+   hum = main['humid']
+   pres = main['pressure']
+   report = data['weather']
+   print("Current Weather Condition in ",city,".....")
+   print("")
+   print("Temperature in (kelvin): ",temp)
+   print("Humidity in percent(%): ",hum)
+   print("Pressure : ",pres)
+   print(f"Weather Report: {report[0]['description']}")
 else:
-    print('Error fetching weather data')
+   print("unable to get details .please try again later")
